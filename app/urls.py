40985f5130f9b66aefe81325d05638b1563a6f6c
marandege <marandege@gmail.com>
2025-06-15 22:03:27 +0200

    move Urls to app

---
+++ app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+# TODO add an app_name
+urlpatterns = [
+    # TODO add names to the urls
+    path('', views.index),
+    path('discover/', views.for_you),
+    path('for-you/', views.for_you),
+    path('login/', views.login),
+    path('signup/', views.signup),
+    path('sport/<sport_name>', views.sport_home),
+    path('sports/', views.sports),
+    path('teams/', views.teams),
+
+]
